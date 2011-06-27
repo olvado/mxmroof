917da9faea04b78483dce7d675010c027641232b
Oli Matthews <olvado@gmail.com>
2011-06-27 14:27:59 +0100

    improved readability of average colour function

--- public/js/application.js
+++ public/js/application.js
@@ -7,10 +7,9 @@ var MXM = (function($,d,undefined){
       clrz.Container = $('#images');
       clrz.Width = this.Container.width();
       clrz.Image_width = this.Width / 5;
-      clrz.count = imgs.length;
       clrz.typeSize();
-      for ( var i = 0; i < clrz.count; i++ ) {
-        clrz.imgTemplate(imgs[i],i);
+      for ( var i = 0; i < imgs.length; i++ ) {
+        clrz.imgTemplate(imgs[i]);
       }
       $(window).resize(function(){
         $('figure').height($('figure:first').width());
@@ -24,7 +23,6 @@ var MXM = (function($,d,undefined){
           $('img',this).fadeOut(1000);
         }
       });
-      
       return this;
     },
     typeSize: function() {
@@ -35,7 +33,7 @@ var MXM = (function($,d,undefined){
       $('p').css('font-size', parent / 17 + 'px');
       $('.req').css('font-size', parent / 17 - 1 + 'px');
     },
-    imgTemplate : function(img,i) {
+    imgTemplate : function(img) {
       var clrx    = this,
           src     = '/imgs/IMG_'+img+'.JPG',
           image   = $('<img>',{
@@ -45,23 +43,24 @@ var MXM = (function($,d,undefined){
           figure  = $('<figure>').html(image).appendTo(this.Container);
       
       image.load(function(){
-        rgb = clrx.getAverageInRGB(d.getElementById('img'+img));
+        rgb = clrx.getAverageColourAsRGB(d.getElementById('img'+img));
         figure.height(figure.width()).css('background-color','rgb('+rgb.r+','+rgb.g+','+rgb.b+')');
       });
       image.hide();
     },
-    getAverageInRGB : function (img) {
+    getAverageColourAsRGB : function (img) {
       var canvas = document.createElement('canvas'),
           context = canvas.getContext && canvas.getContext('2d'),
-          pixelInterval = 5,
-          fallbackRGB = {r:102,g:102,b:102},
-          data, length,
+          rgb = {r:102,g:102,b:102}, // Set a base colour as a fallback for non-compliant browsers
+          pixelInterval = 5, // Rather than inspect every single pixel in the image inspect every 5th pixel
+          count = 0,
           i = -4,
-          rgb = {r:102,g:102,b:102},
-          count = 0;
+          data, length;
 
-      if (!context) { alert('Your browser does not support CANVAS'); return fallbackRGB; }
+      // return the base colour for non-compliant browsers
+      if (!context) { alert('Your browser does not support CANVAS'); return rgb; }
 
+      // set the height and width of the canvas element to that of the image
       var height = canvas.height = img.naturalHeight || img.offsetHeight || img.height,
           width = canvas.width = img.naturalWidth || img.offsetWidth || img.width;
 
@@ -70,20 +69,21 @@ var MXM = (function($,d,undefined){
       try {
         data = context.getImageData(0, 0, width, height);
       } catch(e) {
+        // catch errors - usually due to cross domain security issues
         alert(e);
-        return fallbackRGB;
+        return rgb;
       }
 
       data = data.data;
       length = data.length;
-      while ( (i += pixelInterval * 4) < length ) {
-        ++count;
+      while ((i += pixelInterval * 4) < length) {
+        count++;
         rgb.r += data[i];
         rgb.g += data[i+1];
         rgb.b += data[i+2];
       }
 
-      // ~~ used to floor values
+      // floor the average values to give correct rgb values (ie: round number values)
       rgb.r = Math.floor(rgb.r/count);
       rgb.g = Math.floor(rgb.g/count);
       rgb.b = Math.floor(rgb.b/count);
